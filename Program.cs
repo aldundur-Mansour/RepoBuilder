21383015dee578718a3bce7966b3d99d9cc9269a
Mansour-Aldundur <mansour.aldundur@tuwaiq.edu.sa>
2021-04-19 03:42:08 +0300

    ADD: adding assignment Files

---
+++ Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace cBuilder
+{
+    class Program
+    {
+
+    
+
+        public class gitNewRepoBuilder 
+        {
+            private string _chainOfcommands;  
+             public gitNewRepoBuilder() {
+                 _chainOfcommands = ""; 
+             }
+
+             public gitNewRepoBuilder Init() {
+                 _chainOfcommands = "git init" + " \n";
+                 return this ; 
+             }
+
+             public gitNewRepoBuilder Add(string fileName = "."){
+                 _chainOfcommands += "git add " +  fileName + " \n";
+                 return this ; 
+             }
+
+             public gitNewRepoBuilder Commit(string msg){
+                 _chainOfcommands += "git commit -m "+ msg + " \n";
+                 return this ; 
+             }
+             public gitNewRepoBuilder Branch(string branchName){
+                 _chainOfcommands += "git branch -M " + branchName + " \n"; 
+                 return this ; 
+             }
+
+             public gitNewRepoBuilder Remote (string remoteLink) {
+                 _chainOfcommands += "git remote add origin " + remoteLink + " \n";
+                 return this ;  
+             }
+
+             public gitNewRepoBuilder Push (string branchName){
+                 _chainOfcommands += "git push -u origin "+ branchName + " \n";
+                 return this ;  
+             }
+
+             public string Build() {
+                 return _chainOfcommands ; 
+             }
+
+
+        }
+
+       
+        static void Main(string[] args)
+        {
+            gitNewRepoBuilder newRepo = new gitNewRepoBuilder();
+            string Repo = newRepo
+                        .Init()
+                        .Add()
+                        .Commit("First Commit") 
+                        .Branch("main")
+                        .Remote("https://github.com/aldundur-Mansour/string-check-order")
+                        .Push("main")
+                        .Build();
+
+            Console.WriteLine(Repo) ; 
+        }
+    }
+}
